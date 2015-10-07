a1ebf6895f664384de2b245731baa64384dca3c7
Jun Aruga <happy@oh4u.net>
2015-10-07 15:48:17 +0200

    Update admin.py for Admin page

--- genome/diagnosis/admin.py
+++ genome/diagnosis/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from diagnosis.models import Disease
+from diagnosis.models import Genome
+from diagnosis.models import Snp
+from diagnosis.models import SnpDisease
+
+
+# Customize the layout on admin page.
+class DiseaseAdmin(admin.ModelAdmin):
+    fields = ['name', 'disease_id']
+
+
+admin.site.register(Disease, DiseaseAdmin)
+admin.site.register(Genome)
+admin.site.register(Snp)
+admin.site.register(SnpDisease)
